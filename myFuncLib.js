49b3944b170f7835597ec328655fb19e7583dc78
Simon Lervad <lervad1996@gmail.com>
2019-09-13 12:18:47 +0200

    tredje opgave

--- myFuncLib.js
+++ myFuncLib.js
@@ -13,10 +13,23 @@ console.log(f(x));
 //Assignment JS.Loops.1
 'use srict';
 const fibo = function(n) {
-	if (n === 1 || n === 1)
-		return 1;
+	if (n === 0 || n === 1)
+		return n;
 	else
-		return fibo(n-1)+fibo(n-2)
+		return fibo(n-1)+fibo(n-2);
 }
 let n = Number(prompt('indtast tal her'));
 console.log(fibo(n));
+
+//Assignment JS.Loops.2.2 Functions
+let n = Number(prompt('indtast tal her'));
+let i = 2;
+let isPrime = function(p) {
+	if (n % i === 0) {
+		return false;
+	} else {
+		return true;
+	}
+	i++;
+}
+console.log(isPrime());
