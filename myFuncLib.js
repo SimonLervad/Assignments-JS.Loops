1cfaa165bf71ae7a01f6a0ba04742abf30f843c8
Simon Lervad <lervad1996@gmail.com>
2019-09-13 11:26:16 +0200

    opgave to

--- myFuncLib.js
+++ myFuncLib.js
@@ -1,3 +1,5 @@
+
+//Assignment JS.Loops.0
 'use strict';
 let f = function(x) {
     if (x <= 1)
@@ -7,3 +9,14 @@ let f = function(x) {
 }
 let x = Number(prompt('indtast tal her'));
 console.log(f(x));
+
+//Assignment JS.Loops.1
+'use srict';
+const fibo = function(n) {
+	if (n === 1 || n === 1)
+		return 1;
+	else
+		return fibo(n-1)+fibo(n-2)
+}
+let n = Number(prompt('indtast tal her'));
+console.log(fibo(n));
